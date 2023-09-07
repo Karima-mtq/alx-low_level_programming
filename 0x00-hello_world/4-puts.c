6ff60798a0d4502670810ef9cc209a7fce717f59
karima <karima.mtq@gmail.com>
2023-09-07 06:13:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo '#include <stdio.h>' > my_program.c && echo 'int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }' >> my_program.c && gcc my_program.c -o my_program && ./my_program && rm my_program.c my_program
+#include <stdio.h>
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return 0;
+}

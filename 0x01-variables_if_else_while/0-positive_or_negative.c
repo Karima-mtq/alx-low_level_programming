80f220460c2f362636c7e5c63937b41842288e1d
karima <karima.mtq@gmail.com>
2023-09-08 03:39:17 -0700

    positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,7 @@
 int main(void)
 {
 	int n;
-
-	//Initialize the random number generator with the current time
 	srand(time(0));
-	//Generate a random number between INT_MIN and INT_MAX
 	n = rand() - RAND_MAX / 2;
 
 
@@ -27,4 +24,3 @@ int main(void)
 
 	return(0);
 }
-

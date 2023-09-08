114d5e456f358dc16198a0f677cc75a67c383956
karima <karima.mtq@gmail.com>
2023-09-08 14:12:05 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	char letter = 'a';
-    
-    	while (letter <= 'z')
-    	{
-        	putchar(letter);
+  
+	while (letter <= 'z')
+	{
+		putchar(letter);
 		letter++;
-    	}
-    	putchar('\n');
+	}
+	putchar('\n');
 
 	return (0);
 }

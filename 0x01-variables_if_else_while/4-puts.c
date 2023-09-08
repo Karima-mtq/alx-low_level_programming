2e383b04d75cd165245cc64120fa76ac8318302c
karima <karima.mtq@gmail.com>
2023-09-08 04:45:34 -0700

    last digit

--- 0x01-variables_if_else_while/4-puts.c
+++ 0x01-variables_if_else_while/4-puts.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *    */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    int n;
+    int lastDigit;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    lastDigit = n % 10; 
+
+    if (lastDigit > 5)
+    {
+    	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+    }
+    else if (lastDigit == 0)
+    {
+    	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+    }
+    else
+    {
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+    }
+
+    return (0);
 }

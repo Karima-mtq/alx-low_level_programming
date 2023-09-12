f98e215a3b84e259905c214d47de74b87270ee32
karima <karima.mtq@gmail.com>
2023-09-12 16:57:55 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,26 @@ void print_to_98(int n)
 {
 	int i;
 
-	if ( n < 98)
+	if ( n < 98 )
 	{
 		for (i  = n; i < 98; i++)
 		{
 		printf("%d, ", i);
 		}
-		printf("98\n"); 
+		printf("98\n");
 	}
 	else if (n > 98)
 	{
-		for(i = n; i > 98; i--)
+		for (i = n; i > 98; i--)
 		{
 		printf("%d, ", i);
 		}
 		printf("98\n");
 	}
+	else
+	{
+		printf("98\n");
+	}
+
 
 }

c51c1f9e1de803ef0fbf97395a5f348c79bffed6
karima <karima.mtq@gmail.com>
2023-09-12 12:28:11 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _print_sign - Entry point
+ * @n: the character to be checked
+ * Return: int 1 or 0 or -1
+ */
+
+int print_sign(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		i = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		i = 0;
+	}
+	else
+	{
+		_putchar('-');
+		i = -1;
+	}
+	return (i);
+}

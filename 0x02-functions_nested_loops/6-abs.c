86882355ce5174df9f8d546ea40fecc3d2c78ccb
karima <karima.mtq@gmail.com>
2023-09-12 12:33:06 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: the character to be checked
+ * Return: int n or -n
+ */
+
+int abs(int n)
+{
+	if (n >= 0)
+	{
+		i = n;
+	}
+	else
+	{
+		i = -n
+	}
+	return (i);
+}

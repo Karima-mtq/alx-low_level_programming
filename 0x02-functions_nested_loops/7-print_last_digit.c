a08bfa80753b0aaeb3f64d7e9818891d26f12dac
karima <karima.mtq@gmail.com>
2023-09-12 12:48:19 -0700

    last diigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int print_last_digit - Entry point
+ * print_last_digit - Entry point
  * @n: the character to be checked
  * Return: int
  */
@@ -8,6 +8,11 @@
 int print_last_digit(int n)
 {
 	int lastdigit = n % 10;
+
+	if( lastdigit < 0)
+	{
+		lastdigit *= -1;
+	}
 
 	_putchar(lastdigit + '0');
 

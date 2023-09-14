568748ba23829c2d389fba5678365ba9e9726b5d
karima <karima.mtq@gmail.com>
2023-09-14 06:24:24 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that prints numbers
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}

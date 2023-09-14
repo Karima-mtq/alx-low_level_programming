ad574bd54b1ec2becc22c747c2d67e5272654a6b
karima <karima.mtq@gmail.com>
2023-09-14 06:46:28 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j < 10; j++)
 		{
 		_putchar(j + '0');
+		}
+		for (j = 10; j < 15; j++)
+		{
+			_putchar(j / 10 + '0');
+			_putchar(j * 10 + '0');
 		}
 	_putchar('\n');
 	}

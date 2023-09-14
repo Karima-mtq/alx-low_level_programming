d1d9c141922b5b1fc87a0c151d14855bb8d9370f
karima <karima.mtq@gmail.com>
2023-09-14 12:48:55 -0700

    wright number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+
+void print_number(int n)
+{
+	int divisor = 1;
+	int is_negative = 0;
+
+	if (n < 0)
+	{
+		is_negative = 1;
+		n = -n;
+	}
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	if (is_negative)
+	{
+		_putchar('-');
+	}
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+	divisor /= 10;
+	}
+}

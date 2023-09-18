637cd04f2ddf3d8ea65a7757162f0fb957f65d2a
karima <karima.mtq@gmail.com>
2023-09-18 16:37:37 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,13 @@
 
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i += 2; // Move to the next even-indexed character
-    }
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }

9dc0b92e786b88c13f86d460c910083f46ff4e8f
karima <karima.mtq@gmail.com>
2023-09-19 15:08:46 -0700

    key

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,27 +6,27 @@
  */
 int _atoi(char *s)
 {
-	short boolean;
-	int i, minus, result;
+	short tst;
+	int i, sign, result;
 
-	i = minus = result = boolean = 0;
-	minus = -1;
+	i = sign = result = tst = 0;
+	sign = -1;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			minus *= -1;
+			sign *= -1;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result *= 10;
 			result -= (s[i] - '0');
-			boolean = 1;
+			tst = 1;
 		}
-		else if (boolean == 1)
+		else if (tst == 1)
 			break;
 		i++;
 	}
-	result *= minus;
+	result *= sign;
 	return (result);
 }

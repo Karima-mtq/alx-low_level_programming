d7f2a2ce2c7e61af0686c80694ef45906d07f66d
karima <karima.mtq@gmail.com>
2023-09-20 16:50:29 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,37 @@
 #include "main.h"
+
 /**
- * leet function
- * @str: string
+ * leet - function switch letters
+ * @str: string to be switched
  * Return: string
  */
 
 char *leet(char *str)
 {
 	int i = 0;
+	int j = 0;
+
+	char table[10][2] = {
+		{'a', '4'},
+		{'A', '4'},
+		{'e', '3'},
+		{'E', '3'},
+		{'o', '0'},
+		{'O', '0'},
+		{'t', '7'},
+		{'T', '7'},
+		{'l', '1'},
+		{'L', '1'}
+	};
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			str[i] = '4';
-		}
-		if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == table[j][1])
+			{
+				str[i] = table[j][2];
+			}
 		}
 		i++;
 	}

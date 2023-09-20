5764433834a14826abea3dcf7b23bfdfaa10997b
karima <karima.mtq@gmail.com>
2023-09-20 16:06:32 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,8 @@
 
 /**
  * cap_string - function capitalizes words
- * str: string to be capitalized
- * Return string
+ * @str: string to be capitalized
+ * Return: string
  */
 
 char *cap_string(char *str)
@@ -15,13 +15,28 @@ char *cap_string(char *str)
 			str[i] = str[i] - 32;
 		}
 	i++;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (
+			str[i] == ' ' ||
+			str[i] == '\t' ||
+			str[i] == '\n' ||
+			str[i] == ',' ||
+			str[i] == ';' ||
+			str[i] == '.' ||
+			str[i] == '.' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}'
+			)
 		{
-			if (str[i+1] >= 'a' && str[i+1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i+1] = str[i+1] - 32;
+				str[i + 1] = str[i + 1] - 32;
 			}
 		}
 	i++;

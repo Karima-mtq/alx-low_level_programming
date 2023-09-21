82684164bfc2408af14bdf7c3a596a0b4f6ac39f
karima <karima.mtq@gmail.com>
2023-09-21 04:08:38 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,23 +3,22 @@
 /**
  * rot13 - encodes a string in rot13
  * @s: string to be encoded
- *
  * Return: the resulting string
  */
 char *rot13(char *s)
 {
 	int i, j;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == a[j])
+			if (s[i] == alpha[j])
 			{
-				s[i] = b[j];
+				s[i] = rot[j];
 				break;
 			}
 		}

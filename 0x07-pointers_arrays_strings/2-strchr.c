c4ad75f8caf22ecc44a61392498acddf8b90f6a2
karima <karima.mtq@gmail.com>
2023-09-25 13:32:22 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (s == NULL)
-		return (NULL);
+	int i;
 
-	while (*s != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
+		if ( s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
 	}
+
 	return (NULL);
 }

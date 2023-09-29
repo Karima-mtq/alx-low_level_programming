b11c3b65f22c93ef2bc2548dfc8734e28a6cefe9
karima <karima.mtq@gmail.com>
2023-09-29 15:47:21 -0700

    square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * tst - function tests if n is square
+ * @i: integer
+ * @j: integer
+ * Return: integer
+ */
+
+int tst(int i, int j)
+{
+	if (i * i == j)
+		return (i);
+	if (i * i > j)
+		return (-1);
+	return (tst(i + 1, base));
+}
+
+/**
+ * _sqrt_recursion - square
+ * @n: integer
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (tst(1, n));
+}

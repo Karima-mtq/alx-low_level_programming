04e0921922904f38653c8bfdf663e2d119d73726
karima <karima.mtq@gmail.com>
2023-09-29 15:09:32 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _pow_recursion - calculate x^y
+/**
+ * _pow_recursion - calculate x^y
  * @x: integer
  * @y: integer
  * Return: integer
@@ -8,15 +9,15 @@
 
 int _pow_recursion(int x, int y)
 {
-	if ( y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
 	else
 	{
-		if ( y == 0)
+		if (y == 0)
 			return (1);
 		else
-			return (x * _pow_recursion(x,y - 1));
+			return (x * _pow_recursion(x, y - 1));
 	}
 }

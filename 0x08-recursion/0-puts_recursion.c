7f39341f8b00f2dc936eca3aa5ad8ff5ee7f7610
karima <karima.mtq@gmail.com>
2023-09-29 07:53:02 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a newline
+ * @s: string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}

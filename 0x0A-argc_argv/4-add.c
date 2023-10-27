9990b1f6a5662d421aeb74133fa9df3ecb75d243
karima <karima.mtq@gmail.com>
2023-10-27 16:40:48 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function
+ * @argc: argument
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%i\n", sum);
+	return (0);
+}

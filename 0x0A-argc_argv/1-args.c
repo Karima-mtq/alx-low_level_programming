708b67ee447fecbba85f91a4bcc64672e9a4ec6a
karima <karima.mtq@gmail.com>
2023-10-27 16:27:15 -0700

    name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function
+ * @argc: argument
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

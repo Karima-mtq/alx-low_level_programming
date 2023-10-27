df92f65bc6ee831917875468addfe8c04d8044e8
karima <karima.mtq@gmail.com>
2023-10-27 16:33:37 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function
+ * @argc: argument
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

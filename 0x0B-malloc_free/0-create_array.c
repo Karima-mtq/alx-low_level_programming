ab2778f2edc6e5ec452a100cc6753c7be77f7b22
karima <karima.mtq@gmail.com>
2023-10-31 07:03:39 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,13 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		char *ptr = (char*)malloc(size * sizeof(char));	
+		char *ptr = (char *)malloc(size * sizeof(char));
+
 		for (i = 0; i < size; i++)
 		{
 			*(ptr + i)  = c;
 		}
-	
+
 		return (ptr);
 	}
 }

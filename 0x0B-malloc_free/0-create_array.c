ca1dacec3819e004f14f6b290d2e0eebd891d272
karima <karima.mtq@gmail.com>
2023-10-31 07:07:44 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,20 +10,15 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *ptr = (char *)malloc(size * sizeof(char));
 
-	if (size == 0)
-	{
+	if (size == 0 || ptr == 0)
 		return (NULL);
-	}
-	else
+
+	for (i = 0; i < size; i++)
 	{
-		char *ptr = (char *)malloc(size * sizeof(char));
-
-		for (i = 0; i < size; i++)
-		{
-			*(ptr + i)  = c;
-		}
-
-		return (ptr);
+		*(ptr + i)  = c;
 	}
+
+	return (ptr);
 }

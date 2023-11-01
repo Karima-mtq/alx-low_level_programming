3ca05efd79caa75466c3242d9bf673e0487b96f0
karima <karima.mtq@gmail.com>
2023-11-01 05:37:40 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,11 @@
  * @argv: list
  * Return: 0
  */
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }

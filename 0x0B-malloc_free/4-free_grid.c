bee6b683d22a3b042f986d039184ab2434929084
karima <karima.mtq@gmail.com>
2023-11-01 05:14:17 -0700

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,13 +10,13 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height <= 0)
+	if (grid == NULL)
 		return;
 
 
 	for (i = 0; i < height; i++)
 	{
-		if (grid[i] == NULL)
+		if (grid[i] != NULL)
 		{
 			free(grid[i]);
 		}

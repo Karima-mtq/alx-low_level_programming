80c13a71beafe1824664ff916f599c6081a0d947
karima <karima.mtq@gmail.com>
2023-11-07 03:06:27 -0800

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
- * *new_dog - function
+ * copy_string - function
+ * @src: char
+ * Return: char
+ */
+
+char *copy_string(const char *src)
+{
+	int length = 0, i;
+	char *dest;
+
+	if (src == NULL)
+	{
+		return (NULL);
+	}
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	dest = malloc(length + 1);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - function
  * @name: char
  * @age: float
  * @owner: char
@@ -15,14 +46,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 	if (name == NULL || owner == NULL)
+	{
 		return (NULL);
+	}
 
 	new_dog = malloc(sizeof(dog_t));
-
 	if (new_dog == NULL)
+	{
 		return (NULL);
-
-	new_dog->name = strdup(name);
+	}
+	new_dog->name = copy_string(name);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
@@ -31,14 +64,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog->age = age;
 
-	new_dog->owner = strdup(owner);
+	new_dog->owner = copy_string(owner);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
 	return (new_dog);
 }
-

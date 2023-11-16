c59d6ce2a7053ead3b9661f924e6245984f9d04d
karima <karima.mtq@gmail.com>
2023-11-16 14:57:16 -0800

    add end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node2 = malloc(sizeof(list_t));
-	list_t *node = head;
+	list_t *node = *head;
 
 	if (!head || !node2)
 		return (NULL);
@@ -23,6 +23,12 @@ list_t *add_node_end(list_t **head, const char *str)
 			return (NULL);
 		}
 		node2->len = _strlen(node2->str);
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = node2;
 	}
 	else
 		*head = node2;

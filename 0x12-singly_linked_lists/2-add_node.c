d5c5ef34afb5dd2daf9056caaab3dd2a2b0ec432
karima <karima.mtq@gmail.com>
2023-11-16 14:33:51 -0800

    qdd

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the start
+ * @head: address
+ * @str: str
+ * Return: size
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *head2 = malloc(sizeof(list_t));
+
+	if (!head || head2)
+		return (NULL);
+	if (str)
+	{
+		head2->str = strdup(str);
+		if (head2->str)
+		{
+			free(head2);
+			return (NULL);
+		}
+		head2->len = _strlen(head2->str);
+	}
+
+	head2->next = *head;
+	*head = head2;
+	return (head2);
+}

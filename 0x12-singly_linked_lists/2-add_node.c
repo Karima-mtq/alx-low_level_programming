53576dc4a4361ec6c9d65db6fe7adcb9a0d2630e
karima <karima.mtq@gmail.com>
2023-11-16 14:40:12 -0800

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,12 +10,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *head2 = malloc(sizeof(list_t));
 
-	if (!head || head2)
+	if (!head || !head2)
 		return (NULL);
 	if (str)
 	{
 		head2->str = strdup(str);
-		if (head2->str)
+		if (!head2->str)
 		{
 			free(head2);
 			return (NULL);

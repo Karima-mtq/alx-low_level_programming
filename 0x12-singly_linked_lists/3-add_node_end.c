21e8b6802105b484c75c6f0042653f78029d032b
karima <karima.mtq@gmail.com>
2023-11-16 14:51:19 -0800

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * *add_node_end - function
+ * @head: head
+ * @str: str
+ * Return: list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node2 = malloc(sizeof(list_t));
+	list_t *node = head;
+
+	if (!head || !node2)
+		return (NULL);
+
+	if (str)
+	{
+		node2->str = strdup(str);
+		if (!node2->str)
+		{
+			free(node2);
+			return (NULL);
+		}
+		node2->len = _strlen(node2->str);
+	}
+	else
+		*head = node2;
+	return (node2);
+}

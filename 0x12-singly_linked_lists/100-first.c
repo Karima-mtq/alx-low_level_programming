db79ae6ed3b2a055eb7d634dece6c4f904e91823
karima <karima.mtq@gmail.com>
2023-11-17 03:21:30 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * _constructor - executes before main()
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

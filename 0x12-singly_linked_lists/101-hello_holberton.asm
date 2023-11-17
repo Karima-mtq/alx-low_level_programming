4c4a743495e60006e4f947efcc8091ea59abf6ea
karima <karima.mtq@gmail.com>
2023-11-17 03:40:18 -0800

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,19 @@
-section .data
-    hello_msg db "Hello, Holberton", 10, 0  ; Message to print with newline character
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-section .text
-    global _start
+    section .text
+    global main
+    extern printf
 
-    ; Calling printf function
-_start:
-    mov     rdi, hello_msg  ; Load address of the string to be printed into rdi
-    call    printf          ; Call printf function
+main:
+    push rbp
+    mov rbp, rsp
 
-    ; Exit the program
-    mov     eax, 60         ; System call number for exit
-    xor     edi, edi        ; Exit code 0
-    syscall                 ; Invoke syscall to exit
+    mov rsi, hello
+    mov rdi, format
+    call printf
+
+    pop rbp
+    mov rax, 0
+    ret

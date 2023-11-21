6a2eae0a75dadb1d39d3d7c22eabe6cf3b1dfb2a
karima <karima.mtq@gmail.com>
2023-11-21 11:52:48 -0800

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function
+ * @h: pointer to first node
+ * Return: size
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t l = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

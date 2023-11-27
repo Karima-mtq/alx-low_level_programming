95c049781e70c19bd35e1245a8e888dd72fcb4b5
karima <karima.mtq@gmail.com>
2023-11-27 13:35:21 -0800

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - prints text from a file
+ *
+ * @filename: name of the file
+ * @letters: number of characters to readchars
+ *
+ * Return: actual number of letters read or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int l, chars;
+	char *buffer;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	l = read(file, buffer, letters);
+	if (l == -1)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+
+	chars = write(STDOUT_FILENO, buffer, l);
+
+	free(buffer);
+	close(file);
+	if (chars != l)
+		return (0);
+	return (l);
+}
